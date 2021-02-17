b3664b3a00cbc2dce59710a567456c5061b98882
Daniel Needham <danielneedhamdn@gmail.com>
2021-02-17 21:14:18 +0000

    Added JS function to populate books

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const init = () => {
 const library = document.getElementById("library");
 
 
-const reset = function reset(ev){
+const reset = function reset(ev) {
     ev.preventDefault();
     document.getElementById("form-add").reset();
 }
@@ -17,11 +17,43 @@ const submit = function submit() {
     let author = document.getElementById("author");
     let pages = document.getElementById("pages");
     let read = document.getElementById("read");
-   addBookToLibrary(title, author, pages, read);
-   
+    addBookToLibrary(title, author, pages, read);
+
 }
 
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: "Harry Potter",
+        author: "JK Rowling",
+        pages: 500,
+        readStatus: true
+    },
+    {
+        title: "The Lord of the Rings",
+        author: "JRR Tolkien",
+        pages: 500,
+        readStatus: false,
+    },
+    {
+        title: "The Lord of the Rings",
+        author: "JRR Tolkien",
+        pages: 500,
+        readStatus: false,
+    },
+    {
+        title: "The Lord of the Rings",
+        author: "JRR Tolkien",
+        pages: 500,
+        readStatus: false,
+    },
+    {
+        title: "The Lord of the Rings",
+        author: "",
+        pages: 500,
+        readStatus: false,
+    }
+];
+
 
 function Book(title, author, pages, readStatus) {
     this.title = title
@@ -30,14 +62,35 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus
 }
 
-function addBookToLibrary(title, author, pages, readStatus){
+function addBookToLibrary(title, author, pages, readStatus) {
     let newBook = new Book(title, author, pages, readStatus);
     myLibrary.push(newBook)
 }
 
-function displayBooks(array){
-//START HERE LOOP THORUGH ARRAY CREATING HTML POPULATED 
+//cycles through array
+function render(array) {
+    array.forEach(arrayItem => {
+        const book = createBookTemplate(arrayItem);
+    });
 }
+
+//creates book in DOM from object
+function createBookTemplate(book) {
+    let tile = document.createElement("div");
+    tile.setAttribute("class", "book");
+    library.appendChild(tile);
+    for (let key in book) {
+        if (book[key] === "") continue;
+        let info = document.createElement("p");
+        info.setAttribute("class", "info");
+        if (key === "pages") {
+            info.textContent = book[key] +" pages."
+        } else info.textContent = `${book[key]}`;
+        tile.appendChild(info);
+    }
+}
+
+render(myLibrary);
 
 document.addEventListener("DOMContentLoaded", init);
 
